79a9bd7858a10bf7766ad42fc6fb3c1f5f036c71
Hussein Nour <Hnour99@hotmail.com>
2024-02-11 11:47:45 +0200

    fix

--- Invoice.UI/ActionFilte.cs
+++ Invoice.UI/ActionFilte.cs
@@ -9,16 +9,16 @@ namespace Invoice.UI
     {
         public void OnActionExecuted(ActionExecutedContext context)
         {
+           
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
             if (string.IsNullOrEmpty(context.HttpContext.Session.GetString("token")))
             {
                 context.HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
                 context.Result = new RedirectResult("Account/LogIn");
             }
-        }
-
-        public void OnActionExecuting(ActionExecutingContext context)
-        {
-
         }
     }
 }
